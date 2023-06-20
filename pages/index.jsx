b83191465432cedf53d95d0dccc5cdce15c0aa3c
jeff0518 <cycivs07@icloud.com>
2023-06-20 23:22:26 +0800

    feat: add components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,11 @@
+import {getFeaturedEvents} from '../dummy-data.jsx'
+import EventList from '../components/events/EventList.jsx';
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents()
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
