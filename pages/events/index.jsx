0c906bed5cc3dcbe5ccb9f07b6baa140e9c01895
jeff0518 <cycivs07@icloud.com>
2023-06-22 10:47:41 +0800

    feat: navigating page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,8 +1,22 @@
+import {useRouter} from 'next/router'
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+
 function AllEventsPage() {
+  const router = useRouter()
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+
+    router.push(fullPath)
+  }
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
   );
 }
 
