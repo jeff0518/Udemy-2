b7b97642c7b25739e03ee4473576361635d2cdf0
jeff0518 <cycivs07@icloud.com>
2023-06-26 10:49:09 +0800

    fix: paths for dynamic pages

--- components/helpers/api-util.jsx
+++ components/helpers/api-util.jsx
@@ -1,20 +1,27 @@
 export async function getAllEvents() {
-  const response = await fetch('https://nextjs-course-85312-default-rtdb.asia-southeast1.firebasedatabase.app/events.json')
-  const data = await response.json()
+  const response = await fetch(
+    "https://nextjs-course-85312-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  );
+  const data = await response.json();
 
-  const events = []
+  const events = [];
 
   for (const key in data) {
     events.push({
       id: key,
-      ...data[key]
-    })
+      ...data[key],
+    });
   }
 
-  return events
+  return events;
 }
 
 export async function getFeaturedEvents() {
-  const allEvents = await getAllEvents()
+  const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
 }
