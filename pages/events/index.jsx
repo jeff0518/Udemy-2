01fa69f5c57df8720b64c3a1b71b8f64453c0cae
jeff0518 <cycivs07@icloud.com>
2023-06-26 11:11:18 +0800

    fix: all events

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,16 +1,16 @@
-import {useRouter} from 'next/router'
-import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../components/helpers/api-util";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
-function AllEventsPage() {
-  const router = useRouter()
-  const events = getAllEvents();
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
-    const fullPath = `/events/${year}/${month}`
+    const fullPath = `/events/${year}/${month}`;
 
-    router.push(fullPath)
+    router.push(fullPath);
   }
   return (
     <>
@@ -18,6 +18,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
